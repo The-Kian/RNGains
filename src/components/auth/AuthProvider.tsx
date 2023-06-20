d4a2180ef61e3690d8c4b65eed08994011c0b6af
Kian-SBG <113431242+Kian-SBG@users.noreply.github.com>
2023-06-20 15:30:53 +0100

    restructured db

--- src/components/auth/AuthProvider.tsx
+++ src/components/auth/AuthProvider.tsx
@@ -9,107 +9,107 @@ import { AuthContextType, defaultAuthContext } from "./AuthTypes";
 
 export const AuthContext = createContext(defaultAuthContext);
 
-export function AuthProvider({ children }: ProviderProps) {
+export function AuthProvider({ children }: ProviderProps): JSX.Element {
 	const [user, setUser] = useState<FirebaseAuthTypes.User | null>(null);
-
-  useEffect(() => {
-    auth().onAuthStateChanged(userState => {
-      setUser(userState);
-    })
-  },[])
-
+  
+	useEffect(() => {
+	  auth().onAuthStateChanged(userState => {
+		setUser(userState);
+	  });
+	}, []);
+  
 	const login = async ({
-		email,
-		password,
+	  email,
+	  password,
 	}: {
-		email: string;
-		password: string;
+	  email: string;
+	  password: string;
 	}) => {
-		try {
-			await auth().signInWithEmailAndPassword(email, password);
-		} catch (error) {
-			if (error.code === "auth/user-not-found") {
-				Alert.alert("User not found");
-			}
+	  try {
+		await auth().signInWithEmailAndPassword(email, password);
+	  } catch (error) {
+		if (error.code === "auth/user-not-found") {
+		  Alert.alert("User not found");
 		}
+	  }
 	};
-
+  
 	const register = async ({
-		email,
-		password,
+	  email,
+	  password,
 	}: {
-		email: string;
-		password: string;
-	}) => {
-		try {
-			await auth().createUserWithEmailAndPassword(email, password);
-		} catch (error) {
-			if (error.code === "auth/email-already-in-use") {
-				Alert.alert("That email address is already in use!");
-			}
-			if (error.code === "auth/invalid-email") {
-				Alert.alert("That email address is invalid!");
-			}
+	  email: string;
+	  password: string;
+	}): Promise<void> => {
+	  try {
+		const userCredential = await auth().createUserWithEmailAndPassword(email, password);
+		const user = userCredential.user;
+  
+		await firestore()
+		  .collection('users')
+		  .doc(user?.uid)
+		  .collection('userDetails')
+		  .doc()
+		  .set({
+			displayName: user?.displayName ?? email,
+			email: email,
+		  });
+	  } catch (error) {
+		if (error.code === "auth/email-already-in-use") {
+		  Alert.alert("That email address is already in use!");
 		}
-		try {
-			await firestore()
-			.collection('users')
-			.doc(user?.uid)
-			.collection('details')
-			.add({
-				displayName: user?.displayName ?? email,
-				email: email
-			})
-		} catch (error) {
-			console.log(error.code)
+		if (error.code === "auth/invalid-email") {
+		  Alert.alert("That email address is invalid!");
 		}
+	  }
 	};
-
-	const logout = async () => {
-		try {
-			await auth().signOut();
-		} catch (error) {
-			Alert.alert(error);
-		}
-	};
-
+  
 	const update = async ({
-		displayName,
+	  displayName,
 	}: {
-		displayName: string;
+	  displayName: string;
 	}) => {
-		try {
-			await auth().currentUser?.updateProfile({
-				displayName: displayName,
-			});
-		} catch (error) {
-			Alert.alert(error);
-		} 		
-		try {
-			await firestore()
-			.collection('users')
-			.doc(user?.uid)
-			.collection('details')
-			.add({
-				displayName: user?.displayName ?? user?.email,
-				email: user?.email
-			})
-		} catch (error) {
-			console.log(error.code)
-		}
-    finally{
-      setUser(auth().currentUser)
-    }
+	  try {
+		await auth().currentUser?.updateProfile({
+		  displayName: displayName,
+		});
+	  } catch (error) {
+		Alert.alert(error);
+	  }
+	  try {
+		await firestore()
+		  .collection('users')
+		  .doc(user?.uid)
+		  .collection('userDetails')
+		  .doc()
+		  .set({
+			displayName: user?.displayName ?? user?.email,
+			email: user?.email,
+		  }, { merge: true });
+	  } catch (error) {
+		console.log(error.code);
+	  } finally {
+		setUser(auth().currentUser);
+	  }
 	};
-
+  
+	const logout = async () => {
+	  try {
+		await auth().signOut();
+	  } catch (error) {
+		Alert.alert(error);
+	  }
+	};
+  
 	const value: AuthContextType = {
-		user,
-		setUser,
-		login,
-		register,
-		logout,
-		update,
+	  user,
+	  setUser,
+	  login,
+	  register,
+	  logout,
+	  update,
 	};
-
+  
 	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-};
+  }
+  
