309b4d68d69965444dac7b40c57f2b83558da3dc
Kian-SBG <kian.popat@skybettingandgaming.com>
2023-12-27 14:18:56 +0000

    feat: 🎸 added token caching

--- src/components/messaging/GetDeviceToken.ts
+++ src/components/messaging/GetDeviceToken.ts
@@ -1,16 +1,18 @@
 import saveTokenToDatabase from "./SaveTokenToDatabase";
 import messaging from "@react-native-firebase/messaging";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export function getDeviceToken(userID: string, setToken: (token: string) => void) {
-  console.log("🚀 ~ file: GetToken.ts:6 ~ getDeviceToken ~ getDeviceToken:");
+export async function getDeviceToken(userID: string, setToken: (token: string) => void) {
 
   try {
-    messaging()
-      .getToken()
-      .then((token) => {
-        saveTokenToDatabase(token, userID);
-        setToken(token);
-      });
+    const newToken = await messaging().getToken();
+    const currentToken = await AsyncStorage.getItem('deviceToken');
+
+    if (newToken !== currentToken) {
+      saveTokenToDatabase(newToken, userID);
+      setToken(newToken);
+      await AsyncStorage.setItem('deviceToken', newToken);
+    }
   } catch (error) {
     console.log("🚀 ~ file: GetToken.ts ~ getDeviceToken ~ error", error);
   }
