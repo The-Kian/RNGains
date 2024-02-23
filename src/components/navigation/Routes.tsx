5ae952dbb7fa5a44e2bd75fa93b56616f20aa0d5
Kian-SBG <kian.popat@skybettingandgaming.com>
2024-02-23 11:56:13 +0000

    builds with expo-linking setup

--- src/components/navigation/Routes.tsx
+++ src/components/navigation/Routes.tsx
@@ -1,56 +1,28 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { AuthContext } from "../../context/auth/AuthProvider";
-
-import LoadingOverlay from "../../screens/LoadingOverlay";
-import { NavigationContainer, createNavigationContainerRef } from "@react-navigation/native";
+import { NavigationContainer } from "@react-navigation/native";
 import AuthStack from "./AuthStack";
 import { UserStatsProvider } from "../../context/userStats/UserStatsProvider";
 
 import { ForegoundNotificationReceiver } from "../messaging/FriendRequestNotificationHandler";
 import AppTabs from "./AppTabs";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import LoadingOverlay from "../../screens/LoadingOverlay";
+import * as Linking from "expo-linking";
 
-export const navigationRef = createNavigationContainerRef();
-
-export function navigate(name: string, params: any) {
-  if (navigationRef.isReady()) {
-    console.log("🚀 ~ file: Routes.tsx:17 ~ navigate ~ isReady:")
-    navigationRef.navigate(name, params);
-  }
-}
+export const prefix = Linking.createURL("/");
 
 export default function Routes() {
-  console.log("🚀 ~ file: Routes.tsx:23 ~ Routes ~ Routes: Mounted")
-  const { user } = useContext(AuthContext);
+  const { user, initializing } = useContext(AuthContext);
 
-  
-
-  
-  const checkForNavigation = async () => {
-    // console.log("🚀 ~ file: Routes.tsx:30 ~ checkForNavigation ~ checkForNavigation:")
-    const navigationData = await AsyncStorage.getItem("navigateToScreen");
-    console.log("🚀 ~ file: Routes.tsx:32 ~ checkForNavigation ~ navigationData:", navigationData)
-    if (navigationData) {
-      const { screen, params } = JSON.parse(navigationData);
-      if (screen == "FriendsScreen") {
-        navigate("FriendsTab", { screen, params: { activeView: params.activeView } });
-      }
-      await AsyncStorage.removeItem("navigateToScreen");
-    }
+  const linking = {
+    prefixes: [prefix],
   };
-  checkForNavigation();
-  useEffect(() => {
-    console.log("🚀 ~ file: Routes.tsx:28 ~ useEffect ~ useEffect:")
-    const unsubscribe = navigationRef.addListener("state", checkForNavigation);
-    
-    return unsubscribe;
-  }, []);
 
-  // if (initializing) return <LoadingOverlay message="Loading"></LoadingOverlay>;
+  if (initializing) return <LoadingOverlay message="Loading"></LoadingOverlay>;
 
   return (
     <>
-      <NavigationContainer ref={navigationRef}>
+      <NavigationContainer linking={linking}>
         <ForegoundNotificationReceiver />
         {user ? (
           <UserStatsProvider>
