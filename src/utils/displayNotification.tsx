a664f4e39eba2625628a1131ed317ec891cd9ff4
Kian-SBG <kian.popat@skybettingandgaming.com>
2024-08-30 09:44:26 +0100

    added error alerts

--- src/utils/displayNotification.tsx
+++ src/utils/displayNotification.tsx
@@ -1,9 +1,14 @@
-import notifee from "@notifee/react-native";
+import notifee, { AuthorizationStatus } from "@notifee/react-native";
 import { Alert } from "react-native";
 
-export default async function onDisplayNotifciation() {
-  // await notifee.requestPermission();
+export default async function onDisplayNotification() {
+  const permissionStatus = await notifee.requestPermission();
   Alert.alert("hi");
+
+  if (permissionStatus.authorizationStatus !== AuthorizationStatus.AUTHORIZED) {
+    Alert.alert("Permission denied", "You need to grant notification permissions to receive notifications.");
+    return;
+  }
 
   const channelId = await notifee.createChannel({
     id: "default",
